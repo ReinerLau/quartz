c2866ccd97b8b21d9ec2224ce30851882d9a2b81
ReinerLau <lk850593913@gmail.com>
2024-08-20 14:48:31 +0800

    title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "ReinerLau",
+    pageTitle: "Reiner Lau",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
